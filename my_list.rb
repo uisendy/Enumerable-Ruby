ceccf3ceb82f43a681451516354d3bb55a09c37e
Kenny Elias <109461921+kennyegun24@users.noreply.github.com>
2022-12-24 11:03:54 +0100

    Update file with codes

--- my_list.rb
+++ my_list.rb
@@ -1,1 +1,16 @@
 # Create a class MyList that has an instance variable @list.
+
+require_relative 'my_enumerable'
+
+class MyList
+  def initialize(*list)
+    @list = list
+  end
+
+
+  include MyEnumerable
+
+  def each
+    @list.each { |value| yield value if block_given? }
+  end
+end
